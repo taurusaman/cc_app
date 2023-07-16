f7580d54977c719f93314b15da015d04c32e7714
Aman <amansingh828355@gmail.com>
2023-07-16 22:57:13 +0530

    homescreen carousel

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -15,8 +15,13 @@ class homescreen extends StatefulWidget {
 
 class _homescreenstate extends State<homescreen> {
   int currentIndex = 0;
+  List <Widget> pages = [homescreen(), explore(), search(), profilescreen()];
 
-  List pages = [homescreen(), explore(), search(), profilescreen()];
+  void onItemTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,85 +31,76 @@ class _homescreenstate extends State<homescreen> {
         backgroundColor: Color.fromARGB(255, 199, 91, 52),
         title: Text('Coding Club'),
       ),
-      body: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            CarouselSlider(
-              items: [
-                Image.asset('assets/images/image1.JPG'),
-                Image.asset('assets/images/image2.JPG'),
-                Image.asset('assets/images/image3.jpg'),
-                Image.asset('assets/images/image4.jpg'),
-                Image.asset('assets/images/image5.jpg'),
-                Image.asset('assets/images/image6.jpg'),
-                Image.asset('assets/images/image7.jpg'),
-                Image.asset('assets/images/image8.jpg'),
-              ],
-              options: CarouselOptions(
-                autoPlay: true,
-                enlargeCenterPage: true,
-                aspectRatio: 16 / 7,
-                initialPage: 0,
-                autoPlayCurve: Curves.fastOutSlowIn,
-              ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        IndexedStack(
+          index: currentIndex,
+          children: pages,
+        ),
+        BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          backgroundColor: Color.fromARGB(255, 199, 91, 52),
+          currentIndex: currentIndex,
+          onTap: onItemTapped,
+          fixedColor: Color.fromARGB(255, 199, 91, 52),
+          unselectedItemColor: Colors.black87,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Color.fromARGB(255, 199, 91, 52)
             ),
-            SizedBox(height: 10), // Adjust the size of the indicator space
-
-            Text(
-              'Welcome to coding club community',
-              style: TextStyle(fontSize: 24.0),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              label: 'Explore',
+              backgroundColor: Color.fromARGB(255, 199, 91, 52)
             ),
-            SizedBox(height: 10.0),
-            Container(
-              width: double.infinity,
-              height: 180,
-              child: Card(
-                color: Color.fromARGB(255, 199, 91, 52),
-                elevation: 10,
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'Welcome Coding Club here is our App and we are going rock through this app',
-                    style: TextStyle(fontSize: 30),
-                  ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              backgroundColor: Color.fromARGB(255, 199, 91, 52)
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+              backgroundColor: Color.fromARGB(255, 199, 91, 52)
+            ),
+          ],
+        ),
+        CarouselSlider(
+          items: [
+            Image.asset('assets/images/image1.JPG'),
+            Image.asset('assets/images/image2.JPG'),
+            Image.asset('assets/images/image3.jpg'),
+            Image.asset('assets/images/image4.jpg'),
+            Image.asset('assets/images/image5.jpg'),
+            Image.asset('assets/images/image6.jpg'),
+            Image.asset('assets/images/image7.jpg'),
+            Image.asset('assets/images/image8.jpg'),
+          ],
+          options: CarouselOptions(
+            autoPlay: true,
+            enlargeCenterPage: true,
+            aspectRatio: 16 / 3,
+            initialPage: 0,
+            autoPlayCurve: Curves.fastOutSlowIn,
+          ),
+        ),
+        SizedBox(height: 10), // Adjust the size of the indicator space
+        Container(
+            width: double.infinity,
+            height: 80,
+            child: Card(
+              color: Color.fromARGB(255, 199, 91, 52),
+              elevation: 10,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Welcome to Coding Club',
+                  style: TextStyle(fontSize: 30),
                 ),
               ),
-            ),
-          ])),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.message),
-        backgroundColor: Color.fromARGB(255, 199, 91, 52),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color.fromARGB(255, 199, 91, 52),
-        currentIndex: currentIndex,
-        onTap: (int index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        fixedColor: Color.fromARGB(255, 199, 91, 52),
-        unselectedItemColor: Colors.black87,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            label: 'Explore',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
+            )),
+      ]),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -141,6 +137,11 @@ class _homescreenstate extends State<homescreen> {
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.message),
+        backgroundColor: Color.fromARGB(255, 199, 91, 52),
       ),
     );
   }

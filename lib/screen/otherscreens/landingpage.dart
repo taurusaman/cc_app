2b7722b4c3cb5ca36d4b01ba5ddb6f1db9b37696
taurusaman <amansingh828355@gmail.com>
2023-07-18 20:25:21 +0530

    landing plandingpageage

--- lib/screen/otherscreens/landingpage.dart
+++ lib/screen/otherscreens/landingpage.dart
@@ -11,31 +11,29 @@ class _landingpageState extends State<landingpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black87,
         body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors:[Colors.orange, Colors.black] )
-          ),
-          child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FilledButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/login');
-                        },
-                        child: Text('Login')),
-                        SizedBox(height: 16.0),
-                    FilledButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/register');
-                        },
-                        child: Text('Register'))
-                  ],
-                ),
-              )),
-        ));
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.orange, Colors.black])),
+      child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilledButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    child: Text('Login')),
+                SizedBox(height: 16.0),
+                FilledButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: Text('Register'))
+              ],
+            ),
+          )),
+    ));
   }
 }
